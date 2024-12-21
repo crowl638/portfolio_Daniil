ff36f881c4e3e950db787b309cbe4fadc21cf07c
Даниил <example@yandex.ru>
2024-12-21 14:13:35 +0300

    первый коммит

---
+++ main.js
@@ -0,0 +1,37 @@
+//let a = document.getElementsByClassName("hlink");
+/*
+console.log(a);
+
+ 
+
+let a = prompt("Введите число:");
+let b = prompt("Как вас зовут?");
+
+alert(`Вы ввели число - ${a}. Добро пожаловать, ${b}`);
+*/
+/*
+let a = prompt("Введите длину 1-го катета: ");
+let b = prompt("Введите длину 2-го катета:");
+
+alert(`Гипотенуза равна:  ${Math.sqrt(Math.pow(a,2)+b*b)}`)
+*/
+
+/*let year = prompt("Введите год: ", 2024);
+if (year % 4 == 0 && year % 100 != 0) {
+    alert("True");
+} else {
+    alert("False");}
+*/
+let a = prompt("Введите длину прямоугольника: ")
+let b = prompt("Введите ширину прямоугольника: ")
+
+console.log('Периметр треугольника равен: ', a*2+b*2);
+
+let c = prompt("Введите ваш возраст: ")
+if (c < 18) {
+    console.log("Привет, мой юный друг!")
+} else {
+    console.log("Добро пожаловать!")
+}
+
+
