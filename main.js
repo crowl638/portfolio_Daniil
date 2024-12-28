237dc2c2dbc77c7ba508e0961edf55d7ed1f1cfb
Даниил <example@yandex.ru>
2024-12-28 15:00:40 +0300

    Четвертый коммит

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //let a = document.getElementsByClassName("hlink");
-/*
-console.log(a);
+
+/*console.log(a);
 
  
 
@@ -9,8 +9,8 @@ let b = prompt("Как вас зовут?");
 
 alert(`Вы ввели число - ${a}. Добро пожаловать, ${b}`);
 */
-/*
-let a = prompt("Введите длину 1-го катета: ");
+
+/*let a = prompt("Введите длину 1-го катета: ");
 let b = prompt("Введите длину 2-го катета:");
 
 alert(`Гипотенуза равна:  ${Math.sqrt(Math.pow(a,2)+b*b)}`)
@@ -21,7 +21,7 @@ if (year % 4 == 0 && year % 100 != 0) {
     alert("True");
 } else {
     alert("False");}
-*/
+
 let a = prompt("Введите длину прямоугольника: ");
 let b = prompt("Введите ширину прямоугольника: ");
 
@@ -41,5 +41,34 @@ for(let i=0;i<=n; i++){
         console.log(i);
     }
 }   
+*/
+
+/*let n = prompt("Введите число: ")
+let s = 0;
+for(let i=1; i<=n; i++){
+    if(i%4==0 || i%5==0){
+        s+=i
+    }
+
+}
+console.log(s);
+*/
 
 
+/*let n=prompt("Введите количество чисел: ");
+let a;
+let max=0;
+let min=100000000000;
+for(let i = 0; i<n; i++){
+    a=prompt("Введите число: ");
+    console.log(a)
+    if(a>max){
+        max=a;
+    }
+    if(a<min){
+        min=a;
+    }
+}
+console.log("Миниимальное число равно: ", min);
+console.log("Максимальное число равно: ", max); 
+*/
